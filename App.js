a2299af52fd4e996e4423641699315a0094fc2b5
Rimsha317 <rimshamehmood67@gmail.com>
2020-12-06 23:10:48 +0500

    12th

--- App.js
+++ App.js
@@ -1,21 +1,100 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Form, Item, Input, Label, Button , List,ListItem} from 'native-base'
+import * as firebase from 'firebase'
+import { firebaseConfig } from './config'
+import Constants from 'expo-constants'
 
-export default function App() {
-  return (
+
+firebase.initializeApp(firebaseConfig);
+
+export default class App extends React.Component {
+  state={
+    text: "",
+    mylist:[]
+  }
+  
+    componentDidMount(){
+      const myitems = firebase.database().ref("mywishes");
+      myitems.on("value", datasnap=> {
+       //console.log(Object.values(datasnap.val()))
+       if(datasnap.val()){
+          this.setState({mylist:Object.values(datasnap.val()) })
+       }
+
+      })
+    }
+    saveitem(){
+      //console.log(this.state.text)
+      const mywishes = firebase.database().ref("mywishes");
+      mywishes.push().set({
+        text:this.state.text,
+        time:Date.now()
+      })
+      this.setState({text:""})
+    }
+    removeIt(){
+      firebase.database().ref("mywishes").remove()
+      this.setState({mylist:[{text:"removed Successfully"}] })
+    }
+
+    render(){
+      console.log(this.state)
+      const myitems = this.state.mylist.map(item=>{
+        return(
+          <ListItem style = {{justifyContent:'space-between'}} key={item.time}>
+            <Text>{item.text}</Text>
+            <Text>{new Date(item.time).toDateString()}</Text>
+          </ListItem>
+        )
+      })
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Item floatingLabel>
+        <Label>Add Items</Label>
+            <Input 
+            value={this.state.text}
+            onChangeText={(text)=>this.setState({text})}
+            />
+            </Item>
+            <View style={{flexDirection:"row", padding:20, justifyContent:'space-around'}}>
+              <Button rounded success
+               style={styles.mybtn}
+               onPress={()=>this.saveitem()}>
+                <Text style={styles.text}>Add</Text>
+              </Button>
+              <Button rounded danger
+               style={styles.mybtn}
+               onPress={()=>this.removeIt()}
+               >
+                <Text style={styles.text}>Delete All</Text>
+              </Button>
+            </View>
+            <List>
+              {myitems}
+            </List>
+
+            
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight
   },
+  
+  text: {
+    color:'white',
+    fontSize:20,
+  },
+  mybtn: {
+    padding:10,
+    width:120,
+    justifyContent:'center'
+  }
 });
---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCb7JrNQW4q6Ulxzx-JfYluB3poMW_E66c",
+    authDomain: "fir-6b67c.firebaseapp.com",
+    databaseURL: "https://fir-6b67c.firebaseio.com",
+    projectId: "fir-6b67c",
+    storageBucket: "fir-6b67c.appspot.com",
+    messagingSenderId: "601563528282",
+    appId: "1:601563528282:web:08f4935685922a2492ac63",
+    measurementId: "G-MN9ZTC45EK"
+  };
